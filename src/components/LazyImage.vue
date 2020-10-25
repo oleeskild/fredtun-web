424fbe4cb315b12479998142163f81801f079969
Ole Eskild Steensen <ole.steensen@knowit.no>
2020-10-25 17:08:02 +0100

    Fixed image url for sanity assets

--- src/components/LazyImage.vue
+++ src/components/LazyImage.vue
@@ -1,11 +1,26 @@
 <template>
-    <g-image :src="asset.url" />
+    <g-image :src="urlFor()" />
 </template>
 <script>
+import imageUrlBuilder from '@sanity/image-url';
+const builder = imageUrlBuilder({projectId:"r44b6dmp", dataset:"fredtun"});
 export default {
     props:{
+        _key: {
+            type: String
+        },
+        _type: {
+            type: String
+        },
         asset: {
             type: Object
+        }
+    },
+    mounted:function() {
+    },
+    methods:{
+        urlFor(){
+            return builder.image(this.$props).quality(30).url();
         }
     }
 }
